0fd2f1a0b21924bf2640ffeee9278ba90c05f3d3
Diego Martinez <diegoam6@uci.edu>
2021-07-24 15:05:50 -0700

    Followed the instructions given by codepath to create a tip calculator application. Replaced segmented control with a slider option to allow for a more cuztomizable tip

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    @IBAction func calculateTipSlider(_ sender: Any) {
+        
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tip = bill * Double(tipSlider.value)
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
