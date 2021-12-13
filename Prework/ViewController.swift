8dfeda8b3f79f58e9ea52cc76f3dde785e9d4f8f
Diego Martinez <diegoam6@uci.edu>
2021-12-13 13:46:50 -0800

    Update ViewController.swift

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -17,17 +17,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipSlider: UISlider!
     
+    @IBOutlet weak var tipPercentage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-  
+    
     @IBAction func calculateTipSlider(_ sender: Any) {
-        
+       
         let bill = Double(billAmountTextField.text!) ?? 0
         let tip = bill * Double(tipSlider.value)
         let total = bill + tip
+        let tipPercent = Double(tipSlider.value) * 100
+        tipPercentage.text = String(format:"%.2f", tipPercent)
         tipAmountLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
